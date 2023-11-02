3b6e327858a158074c94629512b75d876ee52ce7
Roy Sandoval <roydavidsandovalviera1@gmail.com>
2023-11-01 21:32:12 -0500

    added a basic gui

--- Main.py
+++ Main.py
@@ -3,17 +3,20 @@ from account import Account
 from ATM import ATM
 from customer import customer
 from savings import savings_account
+import tkinter as tk
+
 
 #Creamos objetos a partir de todas las clases dándoles atributos respectivamente e instanciamos en algunos casos
 customer1_account = Account("Daniel", 50000, "001")
 customer2_account = Account("Sofia", 100000, "002")
 ATM1 = ATM("Laureles", "Bancolombia",customer1_account)
-ATM2 = ATM("San Juan", "Davivienda",customer2_account)
+ATM2 = ATM("San Juan", "Davivienda",customer2_account) 
 customer1 = customer("Daniel", "100", 26, "001", "1234",ATM1,customer1_account,0,"yes")
 customer2 = customer("Sofia", "99", 18, "002", "4321", ATM2,customer2_account, 0, "no")
+customer3 = customer("Juan", "98", 30, "003", "0000", ATM1, customer1_account, 0, "yes")
 savings_account1 = savings_account("Mortgage savings account",0)
 
 #Aplicamos los métodos a cualquiera de los objetos
-customer1.insertcard()
-savings_account1.earn_interest(customer1_account)
-customer2.insertcard()
+def main():
+    customer1.starting_ATM()
+main()
--- customer.py
+++ customer.py
@@ -1,91 +1,191 @@
 from account import Account
-class customer:
-    def __init__(self, name, id, age, cardnumber, pin, atm_instance, account_instance,savings_balance,cuatroxmil):
-        self.name = name
-        self.id = id
-        self.age = age
-        self.cardnumber = cardnumber
-        self.pin = pin
-        self.atm_instance = atm_instance
-        self.account_instance = account_instance
-        self.savings_balance = savings_balance
-        self.cuatroxmil = cuatroxmil
-        self.account = account_instance
+from ATM import ATM
+import tkinter as tk
 
-    def insertcard(self):
-        print("Reading card...")
-        print("Welcome {}!".format(self.name))
-        entered_pin = input("Please enter your PIN:  ")
-        if entered_pin == self.pin:
-            while True:
-                print("\nOptions:")
-                print("1. Transfer")
-                print("2. Deposit")
-                print("3. Withdraw")
-                print("4. Change your PIN")
-                print("5. Save money")
-                print("6. Check balance")
-                print("7. Exit")
-                option = input("Please enter your option (1/2/3/4/5/6):  ")
+root = tk.Tk()
+
+class customer:
+    
+            def __init__(self, name, id, age, cardnumber, pin, atm_instance, account_instance, savings_balance, cuatroxmil):
+                self.name = name
+                self.id = id
+                self.age = age
+                self.cardnumber = cardnumber
+                self.pin = pin
+                self.atm_instance = atm_instance
+                self.account_instance = account_instance
+                self.savings_balance = savings_balance
+                self.cuatroxmil = cuatroxmil
+                self.account = account_instance
                 
-                if option == "1":
+
+            def insertcard(self):
+                def option1():
                     if self.cuatroxmil.lower() == "yes":
-                        amount = int(input("Please enter the amount to transfer:  "))
+                        entry = tk.Entry(root, text='Please enter the amount to transfer: ')
+                        entry.pack()
+                        amount = float(entry.get())
                         transfer_fee = (amount*4)/1000
-                        amount = amount + transfer_fee
+                        amount += transfer_fee
                         self.atm_instance.transfer(amount, self.account_instance)
-                        
                     elif self.cuatroxmil.lower() == "no":
-                        amount = int(input("Please enter the amount to transfer:  "))
+                        entry = tk.Entry(root, text='Please enter the amount to transfer: ')
+                        entry.pack()
+                        amount = float(entry.get())
                         self.atm_instance.transfer(amount, self.account_instance)
-                    
                     else:
-                        print("ERROR: Unknown")
-                        
-                elif option == "2":
-                    amount = int(input("Please enter the amount to deposit:  "))
+                        error_label = tk.Label(root, text="ERROR: Unknown")
+                        error_label.pack()
+
+                def option2():
+                    entry = tk.Entry(root, text='Please enter the amount to deposit: ')
+                    entry.pack()
+                    amount = float(entry.get())
                     self.account_instance.deposit(amount)
-                elif option == "3":
-                        amount = int(input("Please enter the amount to withdraw:  "))
+                    success_label = tk.Label(root, text="Deposit successful")
+                    success_label.pack()
+
+                def option3():
+                    entry = tk.Entry(root, text='Please enter the amount to withdraw: ')
+                    entry.pack()
+                    amount = float(entry.get())
+                    if amount > self.account_instance.balance:
+                        error_label = tk.Label(root, text="Insufficient funds")
+                        error_label.pack()
+                    else:
                         self.account_instance.withdraw(amount)
-                
-                elif option == "4":
+                        success_label = tk.Label(root, text="Withdrawal successful")
+                        success_label.pack()
+
+                def option4():
                     self.changepin()
+
+                def option5():
+                    entry = tk.Entry(root, text='Please enter the amount to save: ')
+                    entry.pack()
+                    save_button = tk.Button(root, text="Save", command=lambda: self.savemoney(entry.get()))
+                    save_button.pack()
+
+                def option6():
+                    balance_label = tk.Label(root, text="Your account balance is {}.".format(self.account_instance.balance))
+                    balance_label.pack()
+
+                def option7():
+                    exit_label = tk.Label(root, text="Exiting the system. Thank you for using our services!")
+                    exit_label.pack()
+                    root.quit()
+
+                name_label = tk.Label(root, text=f'Welcome {self.name} to the ATM, please enter your PIN to continue')
+                name_label.pack()
+                pin_var = tk.StringVar()
+                entry = tk.Entry(root, textvariable=pin_var)
+                entry.pack()
                 
-                elif option == "5":
-                    self.savemoney()
-                
-                elif option == "6":
-                    print('Your account balance is {}.'.format(self.account_instance.balance))
-                
-                elif option == "7":
-                    print("Exiting the system. Thank you for using our services!")
-                    break
-                else:
-                    print("Invalid option")
-        else:
-            print("Invalid PIN")
+                def submit():
+                    entered_pin = entry.get()
+                    global invalid_label_frame
+                    try:
+                         invalid_label_frame.destroy()
+                    except NameError:
+                         pass
+                    if entered_pin == self.pin:
+                            def show_options():
+                                options_label = tk.Label(root, text="\nPlease select an option:",font=('arial', 10, 'bold'))
+                                options_label.pack()
+
+                                button_frame = tk.Frame(root)
+                                button_frame.pack()
+
+                                transfer_button = tk.Button(button_frame, text="Transfer",command=option1)
+                                deposit_button = tk.Button(button_frame, text="Deposit",command=option2)
+                                withdraw_button = tk.Button(button_frame, text="Withdraw",command=option3)
+                                change_pin_button = tk.Button(button_frame, text="Change your PIN",command=option4)
+                                save_money_button = tk.Button(button_frame, text="Save money",command=option5)
+                                check_balance_button = tk.Button(button_frame, text="Check balance",command=option6)
+                                exit_button = tk.Button(button_frame, text="Exit",command=option7)
+                                
+
+                                transfer_button.grid(row=0, column=0, padx=15, pady=10)
+                                deposit_button.grid(row=1, column=0, padx=5, pady=10)
+                                withdraw_button.grid(row=2, column=0)
+                                change_pin_button.grid(row=0, column=1)
+                                save_money_button.grid(row=1, column=1)
+                                check_balance_button.grid(row=2, column=1)
+                                exit_button.grid(row=3, column=0, columnspan=2,pady=20)
+
+                            show_options()
+                            submit_button.config(state='disabled')
     
-    def savemoney(self):
-        amount = int(input("Please enter the amount to save:  "))
-        if amount > 0 and amount < self.account_instance.balance:
-                self.savings_balance += amount
-                self.account_instance.balance -= self.savings_balance
-                print("You have saved ${}, your savings account balance is now ${}.".format(amount, self.savings_balance))
-        else:
-            print("Insufficient funds or the value is negative")
-    
-    def changepin(self):
-        entered_pin = input('Please enter your current PIN: ')
-        if entered_pin == self.pin:
-            while True:
-                new_pin = input('Please enter your new PIN: ')
-                new_pin2 = input("Please enter your new PIN again:  ")
-                if new_pin == new_pin2:
-                    self.pin = new_pin
-                    print("You have changed your PIN successfully")
-                    break
+                    else:
+                        invalid_label_frame = tk.Frame(root)
+                        invalid_label_frame.pack()
+                        invalid_label = tk.Label(invalid_label_frame,text="Invalid PIN")
+                        invalid_label.pack()
+                
+                submit_button = tk.Button(root, text="Submit",command=submit)
+                submit_button.pack()
+
+            def savemoney(self):
+                entry = tk.Entry(root, text='Please enter the amount to save: ')
+                entry.pack()
+                amount = float(entry.get())
+                if amount <= 0:
+                    error_label = tk.Label(root, text="Invalid amount")
+                    error_label.pack()
+                elif amount > self.account_instance.balance:
+                    error_label = tk.Label(root, text="Insufficient funds")
+                    error_label.pack()
                 else:
-                    print("Pin doesn't match")
-        else:
-            raise TypeError("Your pin is incorrect")
+                    self.savings_balance += amount
+                    self.account_instance.balance -= amount
+                    save_label = tk.Label(root, text="You have saved ${}, your savings account balance is now ${}.".format(amount, self.savings_balance))
+                    save_label.pack()
+
+            def changepin(self):
+                pin_var = tk.StringVar()
+                entry = tk.Entry(root, textvariable=pin_var)
+                entry.pack()
+                entered_pin = entry.get()
+                if entered_pin == self.pin:
+                    while True:
+                        new_pin_var = tk.StringVar()
+                        new_pin_entry = tk.Entry(root, textvariable=new_pin_var)
+                        new_pin_entry.pack()
+                        new_pin = new_pin_var.get()
+                        new_pin2_var = tk.StringVar()
+                        new_pin2_entry = tk.Entry(root, textvariable=new_pin2_var)
+                        new_pin2_entry.pack()
+                        new_pin2 = new_pin2_var.get()
+                        if new_pin == new_pin2:
+                            self.pin = new_pin
+                            success_label = tk.Label(root, text="PIN changed successfully")
+                            success_label.pack()
+                            break
+                        else:
+                            error_label = tk.Label(root, text="PINs don't match")
+                            error_label.pack()
+                else:
+                    error_label = tk.Label(root, text="Invalid PIN")
+                    error_label.pack()
+
+            def starting_ATM(self):
+                    
+                    root.title("ATM Banking System")
+                    root.geometry("600x600+500+100")
+
+                    label1 = tk.Label(root, text="Welcome to the ATM, please enter your card to continue", font=('arial', 10, 'bold'), bg='yellow')
+                    label2 = tk.Label(root, text="Reading your card...",state='normal')
+
+                    def show_label2():
+                        label2.pack()
+
+                    def start_process():
+                        show_label2()
+                        self.insertcard()
+
+                    label1.pack()
+
+                    button1 = tk.Button(root, text='Insert card', padx=20,pady=10,command=start_process)
+                    button1.pack()
+                    root.mainloop()
+
